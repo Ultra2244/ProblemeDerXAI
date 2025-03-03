a2c5dec376eec684fd5f3545593dd83a103bdabf
drt58143 <thomas.dreilich99@stud.uni-regensburg.de>
2025-03-03 17:35:18 +0100

    Added iterrative calculation of PGI and PGU

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from openxai.dataloader import ReturnLoaders, ReturnTrainTestX
 from openxai import LoadModel, Explainer, Evaluator
 from openxai import Explainer
 from openxai import Evaluator
-
+from openxai.evaluator import ground_truth_metrics, prediction_metrics, stability_metrics
 
 """
 ---TO DO---
 
-data_name:
-method:
-metric:
+data_name: german/ ...
 ml_model: ann/lr
+method: lime/shap
+metric: PGU/PGI/ ...
 
 ... restliche Dokumentation. Welche Metriken? Kurze Erklärung?
 
 Predictive Faithfulness Metrics
 PGI/PGU:
 """
-def testModelData(data_name, method, metric, model):
+def testModelsData(data_name, method, metric, model):
     # Get training and test loaders
     try:
         trainloader, testloader = ReturnLoaders(data_name, download=True, batch_size=10)
@@ -60,7 +60,15 @@ def testModelData(data_name, method, metric, model):
     explanations = explainer.get_explanations(inputs.float(), preds).detach().numpy()
     print(explanations[0])
 
-    # Load config
+
+    ############### Calculating metrics ###############
+   
+    # Print all possible metrics. 
+    print('Ground truth metrics: ', ground_truth_metrics)
+    print('Prediction metrics: ', prediction_metrics)
+    print('Stability metrics: ', stability_metrics)
+
+    # Load config for prediction_metrics
     param_dict = load_config('experiment_config.json')['evaluators']['prediction_metrics']
     param_dict['inputs'] = inputs
     param_dict['explanations'] = explanations
@@ -68,17 +76,24 @@ def testModelData(data_name, method, metric, model):
     param_dict['perturb_method'] = get_perturb_method(param_dict['std'], data_name)
     del param_dict['std']
 
-    # Print final parameters
+    # Print parameters
     params_preview = [f'{k}: array of size {v.shape}' if hasattr(v, 'shape') else f'{k}: {v}' for k, v in param_dict.items()]
     print(f'{metric.upper()} Parameters\n\n' +'\n'.join(params_preview))
 
-    # Evaluate the metric accross the test inputs/explanations
-    evaluator = Evaluator(model, metric)
-    score, mean_score = evaluator.evaluate(**param_dict)
+    for pm in prediction_metrics:
+        # Metrik setzen
+        metric = pm
 
-    # Print results
-    std_err = np.std(score) / np.sqrt(len(score))
-    print(f"{metric}: {mean_score:.2f}\u00B1{std_err:.2f}")
+        # Evaluate the metric accross the test inputs/explanations
+        evaluator = Evaluator(model, metric)
+        score, mean_score = evaluator.evaluate(**param_dict)
+
+        # Calculate standard erro
+        std_err = np.std(score) / np.sqrt(len(score))
+        
+        # Print results
+        with open("experiment_results.csv", "a") as f:
+            f.write(f"{data_name},{method},{metric},{mean_score:.2f},{std_err:.2f}\n")
 
 if __name__ == '__main__':
-    testModelData('german', 'lime', 'PGI', 'ann')
+    testModelsData('german', 'lime', 'PGI', 'ann')
