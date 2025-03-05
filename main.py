9f7c93565b7c42e917b3985a3111018cb9b52115
drt58143 <thomas.dreilich@stud.uni-regensburg.de>
2025-03-05 16:48:56 +0100

    RIS als stability metric eingeführt

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ metric: PGU/PGI/ ...
 Predictive Faithfulness Metrics
 PGI/PGU:
 """
-def testModelsData(data_name, method, metric, model):
+def testModelsData(data_name, method, model_name):
     # Get training and test loaders
     try:
         trainloader, testloader = ReturnLoaders(data_name, download=True, batch_size=10)
@@ -37,7 +37,7 @@ def testModelsData(data_name, method, metric, model):
     X_train, X_test, feature_metadata = ReturnTrainTestX(data_name, float_tensor=True, return_feature_metadata=True)
 
     # Load pretrained ml model
-    model = LoadModel(data_name, ml_model=model, pretrained=True)
+    model = LoadModel(data_name, ml_model=model_name, pretrained=True)
     print_summary(model, trainloader, testloader)
     preds = model(inputs.float()).argmax(1)
     print(f'First 10 predictions: {preds[:10]}')
@@ -68,7 +68,8 @@ def testModelsData(data_name, method, metric, model):
     print('Prediction metrics: ', prediction_metrics)
     print('Stability metrics: ', stability_metrics)
 
-    # Load config for prediction metrics
+    ### Load config for prediction metrics
+    
     param_dict = load_config('experiment_config.json')['evaluators']['prediction_metrics']
     param_dict['inputs'] = inputs
     param_dict['explanations'] = explanations
@@ -76,7 +77,8 @@ def testModelsData(data_name, method, metric, model):
     param_dict['perturb_method'] = get_perturb_method(param_dict['std'], data_name)
     del param_dict['std']
 
-    # Load config for ground truth metrics
+    ### Load config for ground truth metrics
+    
     # PRA and RC require a slightly different set of parameters, without keys "k" and "AUC2" and "predictions"
     gparam_dict1 = load_config('experiment_config.json')['evaluators']['ground_truth_metrics']
     gparam_dict1['explanations'] = explanations
@@ -84,9 +86,49 @@ def testModelsData(data_name, method, metric, model):
     gparam_dict1['predictions'] = preds
     del gparam_dict2['k'], gparam_dict2['AUC']
 
+    ### Load config for stability metrics
+    
+    # Initialize explainer for stability metrics
+    exp_method = 'grad'
+    exp_param_dict = load_config('experiment_config.json')['explainers'][exp_method]
+    exp_param_dict = fill_param_dict(exp_method, exp_param_dict, X_train)  # if LIME/IG
+    explainer = Explainer(exp_method, model, exp_param_dict)
+
+    sparam_dict = load_config('experiment_config.json')['evaluators']['stability_metrics']
+    sparam_dict['inputs'] = inputs
+    sparam_dict['explainer'] = explainer
+    sparam_dict['feature_metadata'] = feature_metadata
+    sparam_dict['perturb_method'] = get_perturb_method(sparam_dict['std'], data_name)
+    del sparam_dict['std']
+
     # Print parameters
-    params_preview = [f'{k}: array of size {v.shape}' if hasattr(v, 'shape') else f'{k}: {v}' for k, v in param_dict.items()]
-    print(f'{metric.upper()} Parameters\n\n' +'\n'.join(params_preview))
+    # params_preview = [f'{k}: array of size {v.shape}' if hasattr(v, 'shape') else f'{k}: {v}' for k, v in param_dict.items()]
+    # print(f'{metric.upper()} Parameters\n\n' +'\n'.join(params_preview))
+
+    # Save results from Stability Metrics into excel sheet
+    #for sm in stability_metrics:
+    """
+    TODO:
+    1. Die anderen stability metrics einführen
+    2. Code aufräumen:
+        -Redundancies verringern
+        -Häufige literale in variablen abspeichern
+    3. Modeldifferenzierung(ann/lr)
+    4. Explainerdifferenzierung(lime/shap)
+    5. Datasetdifferenzierung
+    """
+    metric = 'RIS'
+
+    # Evaluate the metric accross the test inputs/explanations
+    evaluator = Evaluator(model, metric)
+    score, mean_score = evaluator.evaluate(**sparam_dict)
+
+    # Calculate standard error
+    std_err = np.std(score) / np.sqrt(len(score))
+    
+    # Print results
+    with open("results/experiment_results.csv", "a") as f:
+        f.write(f"{data_name},{method},{metric},{mean_score:.2f},{std_err:.2f}\n")
 
     # Save results from Ground Truth Metrics into excel sheet
     for gtm in ground_truth_metrics:
@@ -122,4 +164,4 @@ def testModelsData(data_name, method, metric, model):
             f.write(f"{data_name},{method},{metric},{mean_score:.2f},{std_err:.2f}\n")
 
 if __name__ == '__main__':
-    testModelsData('german', 'lime', 'PGI', 'lr')
+    testModelsData('adult', 'lime', 'lr')
